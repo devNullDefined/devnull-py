fd8ddb7ed4c09ebe3071eddfff71c112891115ad
Graphity <rohood3@gmail.com>
2020-04-29 19:03:25 +0400

    Now using env vars

--- devnull.py
+++ devnull.py
@@ -5,11 +5,7 @@ import json
 import asyncio
 from random import randint, choice, randrange
 
-with open('config.json', 'r') as cfg:
-    config = json.load(cfg)
-    
-bot = commands.Bot(command_prefix=config['prefix'])
-
+bot = commands.Bot(command_prefix=os.environ['PREFIX'])
 
 @bot.event
 async def on_ready():
@@ -112,4 +108,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
 
-bot.run(config['token'])
+bot.run(os.environ['TOKEN'])
