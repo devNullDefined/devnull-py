0bf735cd5dbcb116b52d56c345c865fff0159ae2
Graphity <rohood3@gmail.com>
2021-08-10 23:52:05 +0400

    Fuck Github API

--- cogs/github.py
+++ cogs/github.py
@@ -1,8 +1,11 @@
-import os
-import discord
-import requests
 from discord.ext import tasks, commands
 from requests.auth import HTTPBasicAuth
+from bs4 import BeautifulSoup
+from datetime import datetime
+import discord
+import requests
+import pytz
+import os
 
 
 class Github(commands.Cog):
@@ -14,49 +17,48 @@ class Github(commands.Cog):
         self.update_github_embeds.start()
 
     def fetch_user_data(self, username):
-        r = requests.get(f'https://api.github.com/users/{username}', auth=self.auth)
-        if r.status_code == 404:
-            return None
-        return r.json()
-
-    def embed_from_data(self, data):
-        name = data['login']
-        r = requests.get(f'https://api.github.com/users/{name}/starred', auth=self.auth)
-        if r.headers.get('Link'):
-            last_page = re.findall(r'\d+', r.headers['Link'])[-1]
-            rl = requests.get(f'https://api.github.com/users/{name}/starred?page={last_page}', auth=self.auth)
-            starred = (int(last_page)-1) * 30 + len(rl.json())
-        else:
-            starred = len(r.json())
+        url = f'https://github.com/{username}'
+        r = requests.get(url, auth=self.auth)
+        if r.status_code != 200:
+            return
+        soup = BeautifulSoup(r.text, 'html.parser')
+        info = soup.find_all('span', class_='text-bold color-text-primary')
+        return {
+            'name': soup.find('title').text.replace(' · GitHub', ''),
+            'about': soup.find('div', class_='p-note user-profile-bio mb-3 js-user-profile-bio f4').text,
+            'followers': info[0].text,
+            'following': info[1].text,
+            'starred': info[2].text,
+            'repos': soup.find('span', class_='Counter').text,
+            'url': url,
+            'avatar': soup.find('a', attrs={'itemprop': 'image'})['href']
+        }
         
-        updated_at = data['updated_at'].replace('T', ' · ')[:-1]
-
-        if data['name']:
-            name = f'{data["name"]} · {name}'
-
+    def embed_from_data(self, data):
         embed = discord.Embed(
-            title=name,
+            title=data['name'],
             type='rich',
-            description=data['bio'] if data['bio'] else discord.Embed.Empty,
-            url=data['html_url'],
+            description=data['about'] if data['about'] else discord.Embed.Empty,
+            url=data['url'],
             colour=0x277ecd
         )
-        embed.set_thumbnail(url=data['avatar_url'])
+        embed.set_thumbnail(url=data['avatar'])
         embed.add_field(
-            name=f'👥 {data["followers"]} followers · {data["following"]} following · ⭐ {starred}',
-            value=f'{data["public_repos"]} public repositories',
+            name=f'👥 {data["followers"]} followers · {data["following"]} following · ⭐ {data["starred"]}',
+            value=f'{data["repos"]} public repositories',
             inline=False
         )
+        time = datetime.now(pytz.timezone('Asia/Tbilisi')).strftime("%Y-%m-%d · %H:%M:%S")
         embed.set_footer(
-                text=f'Updated at: {updated_at}',
+                text=f'Updated at: {time}',
                 icon_url=discord.Embed.Empty
         )
 
         return embed
-        
+            
     @commands.command()
     async def github(self, ctx, username):
-        '''Sends embed for GitHub user'''
+        '''Send embed for GitHub user'''
         data = self.fetch_user_data(username)
         if data:
             embed = self.embed_from_data(data)
@@ -67,7 +69,7 @@ class Github(commands.Cog):
 
     @commands.command()
     async def addgithub(self, ctx, username):
-        '''Creates embed for user in GITHUB_CHANNEL'''
+        '''Create embed for user in GITHUB_CHANNEL'''
         data = self.fetch_user_data(username)
         if data:
             embed = self.embed_from_data(data)
@@ -75,23 +77,20 @@ class Github(commands.Cog):
             await github_channel.send(embed=embed)
         else:
             await ctx.send('User Not Found')
-
+        
     @tasks.loop(hours=12.0)
     async def update_github_embeds(self):
         github_channel = await self.bot.fetch_channel(os.environ['GITHUB_CHANNEL'])
         messages = await github_channel.history(limit=20).flatten()
         for message in messages:
             if message.author == self.bot.user and len(message.embeds) == 1:
-                embed_dict = message.embeds[0].to_dict()
-                update_timestamp = embed_dict['footer']['text'].split(': ')[1].replace(' · ', 'T') + 'Z'
-                username = embed_dict['url'].split('/')[-1]
+                username = message.embeds[0].title.split()[0]
                 new_data = self.fetch_user_data(username)
                 if not new_data:
                     await github_channel.send(f'Failed updating data for user: **{username}**')
                 else:
-                    if new_data['updated_at'] != update_timestamp:
-                        new_embed = self.embed_from_data(new_data)
-                        await message.edit(embed=new_embed)
+                    new_embed = self.embed_from_data(new_data)
+                    await message.edit(embed=new_embed)
 
 
 def setup(bot):
