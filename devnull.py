56a6128c27af35762693726fd0e8527ced61d25f
Graphity <rohood3@gmail.com>
2021-08-10 16:36:57 +0400

    Helpful messages on command errors PepeLaugh

--- devnull.py
+++ devnull.py
@@ -1,5 +1,4 @@
 from discord.ext import commands
-from discord.ext.commands import CommandNotFound
 import discord
 import asyncio
 import json
@@ -19,8 +18,16 @@ async def on_ready():
 
 @bot.event
 async def on_command_error(ctx, error):
-    if isinstance(error, CommandNotFound):
-        await ctx.send(f'მაგაზე პასუხი არ მაქვს, მგონი პრეფიქსი აგერია {bot.get_emoji(645706036292091904)}')
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send(bot.get_emoji(694341216267010078))
+    elif isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send(f'Missing required argument {bot.get_emoji(667775700538753034)}')
+    elif isinstance(error, commands.BadArgument):
+        await ctx.send(f'Absolute bullshit argument {bot.get_emoji(629773774920744990)}')
+    elif isinstance(error, commands.MissingPermissions):
+        await ctx.send(f'If you think you can do that... {bot.get_emoji(668526049277247498)}')
+    elif isinstance(error, commands.NotOwner):
+        await ctx.send(f'You are not that guy pal {bot.get_emoji(694344505431949323)}')
     raise error
 
 
